e614f72823d8d01a46c94245435dfe74b817f092
CreaterYDJ <921104178@qq.com>
2019-11-19 17:12:12 -0800

    week12 guard processor

---
+++ week12/code/guard/6-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+void init_deamon(void);
+int main()
+{
+	FILE *fp;
+	time_t t;
+	init_deamon();
+	while(1)
+	{
+		sleep(10);
+		if((fp==fopen("6-8.log","a"))>=0)
+		{
+			t=time(0);
+			fprintf(fp,"guard process still running,time is:%s",asctime(localtime(&t)));
+			fclose(fp);
+		}
+	}
+}
